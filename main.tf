1b982b750dafb9821190b58d3936c08190170b3a
Rajesh Potru <46541011+rpotru@users.noreply.github.com>
2022-12-30 06:58:23 -0500

    fix typo

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ module "blog_sg" {
   ingress_rules       = ["http-80-tcp", "https-8443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules       = ["all-all "]
+  egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 
 }
