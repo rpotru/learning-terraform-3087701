5b5f9bc702945bd4f7b7df852a1c334e499bd9b7
Rajesh Potru <46541011+rpotru@users.noreply.github.com>
2022-12-30 07:31:33 -0500

    fixed error

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "aws_security_group" "blog" {
   tags = {
     Terraform = "true"
   }
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.vpc.public_subnets[0]
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
